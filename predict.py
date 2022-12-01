03169c0a799bc388b2ff4aa8d3e8b210ec803e4c
parin.kit <parin.kit@proxumer.com>
2022-12-01 14:29:29 +0700

    font_manager._rebuild()

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 from skimage import io
 import cv2
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+font_manager._rebuild()
 plt.rcParams['font.family'] = 'Tahoma'
 
 import onnxruntime as rt
